f2ae35584e6e478beaa9743b8c9451ef79f33612
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-06-01 14:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function name() {
+return new Promise((res,rej)=>{
+	setTimeout(()=>res("Hello, world!"),1000)
+})
+}
+function name1() {
+return new Promise((res,rej)=>{
+	setTimeout(()=>res("Hello, world!"),2000)
+})
+}
+function name2() {
+return new Promise((res,rej)=>{
+	setTimeout(()=>res("Hello, world!"),3000)
+})
+}
+function name3() {
+return new Promise((res,rej)=>{
+	setTimeout(()=>res("Hello, world!"),4000)
+})
+}
+function name4() {
+return new Promise((res,rej)=>{
+	setTimeout(()=>res("Hello, world!"),5000)
+})
+}
+window.promises.push(name,name1,name2,name3,name4);
